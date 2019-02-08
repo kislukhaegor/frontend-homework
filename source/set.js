e2e5ead943b4d5622b7f30d6123bd0e471476058
CHSV <kislukhaegor@gmail.com>
2019-02-09 01:01:12 +0300

    refactoring; add hasOwnProperty instead operator in

--- source/set.js
+++ source/set.js
@@ -2,19 +2,19 @@
 
 const set = (object, pathToField, value) => {
 	if (!pathToField) {
-		object = value;
 		return value;
 	}
+
 	if (object === undefined) {
 		object = {};
 	}
 
-	let arr = pathToField.split('.');
-	let last_field = arr.pop();
+	let arr_of_fields = pathToField.split('.');
+	let last_field = arr_of_fields.pop();
 
 	// если в строке отсутсвует точка в начале, то начинать нужно с первого элемента массива	
 	let start_index = 0;
-	if (!arr[0]) {
+	if (!arr_of_fields[0]) {
 		start_index = 1;
 	}
 
@@ -22,13 +22,13 @@ const set = (object, pathToField, value) => {
 		if (index < start_index) {
 			return accumulator;
 		}
-		if (!(currentValue in accumulator)) {
+		if (!accumulator.hasOwnProperty(currentValue)) {
 			accumulator[currentValue] = {};
 		}
 		return accumulator[currentValue];
 	}
 
-	arr.reduce(reduce_impl, object)[last_field] = value;
+	arr_of_fields.reduce(reduce_impl, object)[last_field] = value;
 
 	return object;
 }
